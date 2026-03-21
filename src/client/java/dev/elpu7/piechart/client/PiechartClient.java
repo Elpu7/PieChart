e86ae2af2df7e9b536fecd20237e8426f9cd402b
Emil <Emil.lahdemaki@outlook.com>
2026-03-21 15:36:04 +0200

    init commit

---
+++ src/client/java/dev/elpu7/piechart/client/PiechartClient.java
@@ -0,0 +1,10 @@
+package dev.elpu7.piechart.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PiechartClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/elpu7/piechart/Piechart.java
@@ -0,0 +1,10 @@
+package dev.elpu7.piechart;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Piechart implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
